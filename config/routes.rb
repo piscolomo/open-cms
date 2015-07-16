c1f61908abc66396cc3917578411ff2045f19206
Julio Lopez <ljuliom@gmail.com>
2015-07-15 22:12:28 -0500

    more changes in admin views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
 
   namespace :admin do
     resources :categories
@@ -14,9 +15,6 @@ Rails.application.routes.draw do
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
