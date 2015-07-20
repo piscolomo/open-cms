e1c71dc51093aa5de1fb807548c958650e4d6d42
Julio Lopez <ljuliom@gmail.com>
2015-07-20 02:02:39 -0500

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714050010) do
+ActiveRecord::Schema.define(version: 20150720070011) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,5 +30,15 @@ ActiveRecord::Schema.define(version: 20150714050010) do
   end
 
   add_index "posts", ["category_id"], name: "index_posts_on_category_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
