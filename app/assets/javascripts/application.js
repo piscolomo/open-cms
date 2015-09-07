47ca6e8db250e3a285b5b3f19dd1cd152ae9fe85
Julio Lopez <ljuliom@gmail.com>
2015-09-07 17:56:31 -0500

    configured bower and installed dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
+//= require medium-editor/dist/js/medium-editor
+//= require handlebars/handlebars.runtime.min
+//= require jquery-sortable/source/js/jquery-sortable-min
+//= require blueimp-file-upload/js/vendor/jquery.ui.widget
+//= require blueimp-file-upload/js/jquery.iframe-transport
+//= require blueimp-file-upload/js/jquery.fileupload
+//= require medium-editor-insert-plugin/dist/js/medium-editor-insert-plugin.min
 //= require app.min
 //= #require turbolinks
 //= require_tree .
